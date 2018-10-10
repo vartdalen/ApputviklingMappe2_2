14e98398d68d289c30f3458f79b0759b0bf94f7e
Vartdalen <viktor.v.johansen@hotmail.com>
2018-10-10 16:45:54 +0200

    v1.8: regex på registreringsside

--- ApputviklingMappe2/app/src/main/java/vica/apputviklingmappe2/ActivitySignup.java
+++ ApputviklingMappe2/app/src/main/java/vica/apputviklingmappe2/ActivitySignup.java
@@ -3,16 +3,14 @@ package vica.apputviklingmappe2;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.content.ContentValues;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toolbar;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class ActivitySignup extends Activity {
 
@@ -28,28 +26,28 @@ public class ActivitySignup extends Activity {
     public final static String USER_PHONE = "Phonenumber";
     public final static String USER_PASSWORD = "Password";
 
-    private EditText firstname;
-    private EditText lastname;
+    private EditText firstName;
+    private TextView firstNameFeedback;
+    private EditText lastName;
+    private TextView lastNameFeedback;
+    private EditText phone;
+    private TextView phoneFeedback;
     private EditText email;
-    private EditText phonenr;
+    private EditText emailConfirm;
+    private TextView emailConfirmFeedback;
     private EditText password;
-
-    private Button signup_button;
+    private EditText passwordConfirm;
+    private TextView passwordFeedback;
+    private Button signupButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        firstname = (EditText)findViewById(R.id.signup_first_name);
-//        lastname = (EditText)findViewById(R.id.lastname);
-//        email = (EditText)findViewById(R.id.signup_email);
-//        phonenr = (EditText)findViewById(R.id.phonenr);
-//        password  = (EditText)findViewById(R.id.signup_password);
-
-        signup_button = (Button)findViewById(R.id.signup_button);
-
         setupToolbar();
+        setupFields();
+        setupListeners();
     }
 
     private void setupToolbar() {
@@ -68,6 +66,39 @@ public class ActivitySignup extends Activity {
                 return true;
             }
         });
+    }
+
+    private void setupListeners() {
+        OnTextChangedListener firstNameOnTextChangedListener = new OnTextChangedListener(firstName, null, firstNameFeedback, "^[A-Z][A-Za-z '-]+$", getString(R.string.invalid_first_name), null);
+        firstName.addTextChangedListener(firstNameOnTextChangedListener);
+        OnTextChangedListener lastNameOnTextChangedListener = new OnTextChangedListener(lastName, null, lastNameFeedback, "^[A-Z][A-Za-z '-]+$", getString(R.string.invalid_last_name), null);
+        lastName.addTextChangedListener(lastNameOnTextChangedListener);
+        OnTextChangedListener phoneOnTextChangedListener = new OnTextChangedListener(phone, null, phoneFeedback, "^[0-9]{8}$", getString(R.string.invalid_phone), null);
+        phone.addTextChangedListener(phoneOnTextChangedListener);
+        OnTextChangedListener emailOnTextChangedListener = new OnTextChangedListener(email, emailConfirm, emailConfirmFeedback, "^(([^<>()\\[\\]\\\\.,;:\\s@\"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@\"]+)*)|(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$", getString(R.string.invalid_email1), getString(R.string.invalid_email12));
+        email.addTextChangedListener(emailOnTextChangedListener);
+        OnTextChangedListener emailConfirmOnTextChangedListener = new OnTextChangedListener(emailConfirm, email, emailConfirmFeedback, "^(([^<>()\\[\\]\\\\.,;:\\s@\"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@\"]+)*)|(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$", getString(R.string.invalid_email1), getString(R.string.invalid_email12));
+        emailConfirm.addTextChangedListener(emailConfirmOnTextChangedListener);
+        OnTextChangedListener passwordOnTextChangedListener = new OnTextChangedListener(password, passwordConfirm, passwordFeedback, "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,20}$", getString(R.string.invalid_password1), getString(R.string.invalid_password2));
+        password.addTextChangedListener(passwordOnTextChangedListener);
+        OnTextChangedListener passwordConfirmOnTextChangedListener = new OnTextChangedListener(passwordConfirm, password, passwordFeedback, "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,20}$", getString(R.string.invalid_password1), getString(R.string.invalid_password2));
+        passwordConfirm.addTextChangedListener(passwordConfirmOnTextChangedListener);
+    }
+
+    private void setupFields() {
+        firstName = (EditText)findViewById(R.id.signup_first_name);
+        firstNameFeedback = (TextView)findViewById(R.id.signup_first_name_feedback);
+        lastName = (EditText)findViewById(R.id.signup_last_name);
+        lastNameFeedback = (TextView)findViewById(R.id.signup_last_name_feedback);
+        phone = (EditText)findViewById(R.id.signup_phone);
+        phoneFeedback = (TextView)findViewById(R.id.signup_phone_feedback);
+        email = (EditText)findViewById(R.id.signup_email);
+        emailConfirm = (EditText)findViewById(R.id.signup_email_confirm);
+        emailConfirmFeedback = (TextView)findViewById(R.id.signup_email_confirm_feedback);
+        password  = (EditText)findViewById(R.id.signup_password);
+        passwordFeedback = (TextView)findViewById(R.id.signup_password_feedback);
+        passwordConfirm  = (EditText)findViewById(R.id.signup_password_confirm);
+        signupButton = (Button)findViewById(R.id.signup_button);
     }
 
     public void quit() {
@@ -90,34 +121,28 @@ public class ActivitySignup extends Activity {
         confirm_quit.show();
     }
 
-    public void signup_button(View v){
-        ContentValues values = new ContentValues();
-
-        String firstnameV = firstname.getText().toString();
-//        String lastnameV = lastname.getText().toString();
+//    public void signup_button(View v){
+//        ContentValues values = new ContentValues();
+//
+//        String firstnameV = firstName.getText().toString();
+//        String lastnameV = lastName.getText().toString();
 //        String emailV = email.getText().toString();
-//        String addressV = address.getText().toString();
-//        String phonenrV = phonenr.getText().toString();
+//        String phonenrV = phone.getText().toString();
 //        String passwordV = password.getText().toString();
-//        String zipcodeV = zipcode.getText().toString();
-//        String zipareaV = ziparea.getText().toString();
-
-        values.put(USER_FIRSTNAME, firstnameV);
+//
+//        values.put(USER_FIRSTNAME, firstnameV);
 //        values.put(USER_LASTNAME, lastnameV);
 //        values.put(USER_EMAIL, emailV);
-//        values.put(USER_ADDRESS, addressV);
 //        values.put(USER_PHONE, phonenrV);
 //        values.put(USER_PASSWORD, passwordV);
-//        values.put(ZIP_CODE, zipcodeV);
-//        values.put(AREA, zipareaV);
-
-        Uri uri = getContentResolver().insert( CONTENT_URI, values);
-        firstname.setText("");
-//        lastname.setText("");
+//
+//        Uri uri = getContentResolver().insert( CONTENT_URI, values);
+//        firstName.setText("");
+//        lastName.setText("");
 //        email.setText("");
-//        phonenr.setText("");
+//        phone.setText("");
 //        password.setText("");
-
-        Toast.makeText(this, "Registered!", Toast.LENGTH_SHORT).show();
-    }
+//
+//        Toast.makeText(this, "Registered!", Toast.LENGTH_SHORT).show();
+//    }
 }
