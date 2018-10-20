9f3296e0ac31852845732a096d00b9b9ce9b85fb
Vartdalen <viktor.v.johansen@hotmail.com>
2018-10-20 16:59:57 +0200

    date & time pickers

--- ApputviklingMappe2/app/src/main/java/vica/apputviklingmappe2/ActivityBooking.java
+++ ApputviklingMappe2/app/src/main/java/vica/apputviklingmappe2/ActivityBooking.java
@@ -1,9 +1,12 @@
 package vica.apputviklingmappe2;
 
-import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
 import android.util.SparseBooleanArray;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,18 +14,31 @@ import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toolbar;
 
 import java.util.ArrayList;
 
-public class ActivityBooking extends Activity {
+public class ActivityBooking extends FragmentActivity implements TimePickerDialog.OnTimeSetListener, DatePickerDialog.OnDateSetListener {
     private Toolbar toolbar;
     private Helper helper;
     private Session session;
     private DB db;
 
+    private ImageButton buttonDate;
+    private ImageButton buttonTime;
     private Button buttonNext;
+    private TextView textDate;
+    private TextView textTime;
+    private int hours;
+    private int minutes;
+    private int day;
+    private int month;
+    private int year;
 
     private ListView restaurantList;
     private ArrayAdapter<String> listAdapter;
@@ -40,6 +56,41 @@ public class ActivityBooking extends Activity {
         setupToolbar();
         setupFields();
         populateRestaurantList();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        if(!textDate.getText().toString().equals("Choose a date") && !textTime.getText().toString().equals("Choose a time")) {
+            savedInstanceState.putString("restaurant", restaurantList.getSelectedItem().toString());
+            savedInstanceState.putString("date", textDate.getText().toString());
+            savedInstanceState.putString("time", textTime.getText().toString());
+        }
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int outYear, int outMonth, int outDay) {
+        year = outYear;
+        month = outMonth;
+        day = outDay;
+        String date = outDay + "/" + outMonth + "/" + outYear;
+        textDate.setText(date);
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int outHours, int outMinutes) {
+        hours = outHours;
+        minutes = outMinutes;
+        String time = outHours + ":";
+        if(outHours < 10) {
+            time = "0" + outHours + ":";
+        }
+        if (outMinutes < 10) {
+            time = time + "0" + outMinutes;
+        } else {
+            time = time + outMinutes;
+        }
+        textTime.setText(time);
     }
 
     private void populateRestaurantList() {
@@ -68,11 +119,31 @@ public class ActivityBooking extends Activity {
         restaurantList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if(!restaurantList.isItemChecked(restaurantList.getPositionForView(view))) {
-                    restaurantList.setItemChecked(restaurantList.getPositionForView(view), false);
-                } else {
-                    restaurantList.setItemChecked(restaurantList.getPositionForView(view), true);
-                }
+            if(!restaurantList.isItemChecked(restaurantList.getPositionForView(view))) {
+                restaurantList.setItemChecked(restaurantList.getPositionForView(view), false);
+            } else {
+                restaurantList.setItemChecked(restaurantList.getPositionForView(view), true);
+            }
+            }
+        });
+
+        textDate = (TextView) findViewById(R.id.restaurant_date);
+        buttonDate = (ImageButton) findViewById(R.id.restaurant_date_button);
+        buttonDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment dateFragment = new FragmentDatePicker();
+                dateFragment.show(getSupportFragmentManager(), "datePicker");
+            }
+        });
+
+        textTime = (TextView) findViewById(R.id.restaurant_time);
+        buttonTime = (ImageButton) findViewById(R.id.restaurant_time_button);
+        buttonTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment timeFragment = new FragmentTimePicker();
+                timeFragment.show(getSupportFragmentManager(), "timePicker");
             }
         });
 
@@ -80,19 +151,18 @@ public class ActivityBooking extends Activity {
         buttonNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                int indexSelected = -1;
                 SparseBooleanArray checked = restaurantList.getCheckedItemPositions();
-                ArrayList<String> temp = new ArrayList<>();
                 for(String s : restaurantListArray) {
                     if(checked.get(restaurantListArray.indexOf(s))) {
-                        db.deleteRestaurant(ActivityBooking.this, helper.stringParser(s));
-                    } else {
-                        temp.add(s);
+                        indexSelected = restaurantListArray.indexOf(s);
                     }
                 }
-                restaurantListArray.clear();
-                restaurantListArray.addAll(temp);
-                listAdapter.notifyDataSetChanged();
-                restaurantList.setAdapter(listAdapter);
+                if (indexSelected > -1 && !textDate.getText().toString().equals("Choose a date") && !textTime.getText().toString().equals("Choose a time")) {
+                    System.out.println(restaurantList.getItemAtPosition(indexSelected).toString());
+                    System.out.println(textDate.getText().toString());
+                    System.out.println(textTime.getText().toString());
+                }
             }
         });
     }
@@ -132,5 +202,4 @@ public class ActivityBooking extends Activity {
             }
         });
     }
-
 }
