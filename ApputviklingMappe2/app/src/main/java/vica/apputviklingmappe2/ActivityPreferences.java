10f99e52864a5a56a900a20c5b96ffe8a8ea2a14
Vartdalen <viktor.v.johansen@hotmail.com>
2018-10-27 19:39:13 +0200

    listener på booleans i prefs som slår av periodic service hvis begge bli satt til false

--- ApputviklingMappe2/app/src/main/java/vica/apputviklingmappe2/ActivityPreferences.java
+++ ApputviklingMappe2/app/src/main/java/vica/apputviklingmappe2/ActivityPreferences.java
@@ -1,19 +1,49 @@
 package vica.apputviklingmappe2;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.widget.Toast;
 import android.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 
-public class ActivityPreferences extends PreferenceActivity {
+public class ActivityPreferences extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    Helper helper;
+    SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(null);
         addPreferencesFromResource(R.xml.activity_preferences);
         setupToolbar();
+        prefs = PreferenceManager.getDefaultSharedPreferences(ActivityPreferences.this);
+        helper = new Helper();
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if(!prefs.getBoolean(getString(R.string.personal_reminder), false) && !prefs.getBoolean(getString(R.string.notify_friends), false)) {
+            helper.stoppPeriodisk(ActivityPreferences.this);
+            Toast.makeText(ActivityPreferences.this, getString(R.string.periodic_disabled), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+
+    }
+
+    @Override
+    public void onPause() {
+        getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+        super.onPause();
     }
 
     private void setupToolbar() {
