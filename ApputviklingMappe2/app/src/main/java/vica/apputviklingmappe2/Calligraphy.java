6752aae6345f9219b27feea26cdd5aaeee33771d
Carlo <s315613@stud.hioa.no>
2018-10-28 20:11:37 +0100

    init

---
+++ ApputviklingMappe2/app/src/main/java/vica/apputviklingmappe2/Calligraphy.java
@@ -0,0 +1,22 @@
+package vica.apputviklingmappe2;
+
+import android.app.Application;
+
+import io.github.inflationx.calligraphy3.CalligraphyConfig;
+import io.github.inflationx.calligraphy3.CalligraphyInterceptor;
+import io.github.inflationx.viewpump.ViewPump;
+
+public class Calligraphy extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ViewPump.init(ViewPump.builder()
+                .addInterceptor(new CalligraphyInterceptor(
+                        new CalligraphyConfig.Builder()
+                                .setDefaultFontPath("fonts/sans-serif-smallcaps")
+                                .setFontAttrId(R.attr.fontPath)
+                                .build()))
+                .build());
+    }
+}
